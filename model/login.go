b660f1c35ac8eee65c41f4f8d43ebd410e92062c
armandasuryan <armanda.surya97@gmail.com>
2024-09-03 13:49:48 +0700

    modify type

--- model/login.go
+++ model/login.go
@@ -1,25 +1,27 @@
 package model
 
-type Login struct {
-	UserName string `json:"user_name" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
+type (
+	Login struct {
+		UserName string `json:"user_name" validate:"required"`
+		Password string `json:"password" validate:"required"`
+	}
 
-type VerifyOTP struct {
-	UserName string `json:"user_name" validate:"required"`
-	OTPCode  int    `json:"otp" validate:"required"`
-}
+	VerifyOTP struct {
+		UserName string `json:"user_name" validate:"required"`
+		OTPCode  int    `json:"otp" validate:"required"`
+	}
 
-type OTP struct {
-	OTP      int    `json:"otp"`
-	TimeLeft string `json:"time_left"`
-}
+	OTP struct {
+		OTP      int    `json:"otp"`
+		TimeLeft string `json:"time_left"`
+	}
 
-type ResponseLogin struct {
-	ID       int    `json:"id"`
-	UserName string `json:"user_name"`
-	RoleName string `json:"role_name"`
-	Name     string `json:"employee_name"`
-	Email    string `json:"email"`
-	Token    string `json:"token"`
-}
+	ResponseLogin struct {
+		ID       int    `json:"id"`
+		UserName string `json:"user_name"`
+		RoleName string `json:"role_name"`
+		Name     string `json:"employee_name"`
+		Email    string `json:"email"`
+		Token    string `json:"token"`
+	}
+)
--- utils/response.go
+++ utils/response.go
@@ -1,39 +1,41 @@
 package utils
 
-type ResponseMeta struct {
-	StatusCode int         `json:"status_code"`
-	Message    string      `json:"message"`
-	Error      string      `json:"error,omitempty"`
-	Data       interface{} `json:"data,omitempty"`
-	Meta       Meta        `json:"meta,omitempty"`
-}
+type (
+	ResponseMeta struct {
+		StatusCode int         `json:"status_code"`
+		Message    string      `json:"message"`
+		Error      string      `json:"error,omitempty"`
+		Data       interface{} `json:"data,omitempty"`
+		Meta       Meta        `json:"meta,omitempty"`
+	}
 
-type ResponseData struct {
-	StatusCode int         `json:"status_code"`
-	Message    string      `json:"message"`
-	Error      string      `json:"error,omitempty"`
-	Data       interface{} `json:"data,omitempty"`
-}
+	ResponseData struct {
+		StatusCode int         `json:"status_code"`
+		Message    string      `json:"message"`
+		Error      string      `json:"error,omitempty"`
+		Data       interface{} `json:"data,omitempty"`
+	}
 
-type ResponseValidator struct {
-	StatusCode int      `json:"status_code"`
-	Message    string   `json:"message"`
-	Error      []string `json:"error,omitempty"`
-}
+	ResponseValidator struct {
+		StatusCode int      `json:"status_code"`
+		Message    string   `json:"message"`
+		Error      []string `json:"error,omitempty"`
+	}
 
-type Meta struct {
-	Pagination Pagination `json:"pagination"`
-}
+	Meta struct {
+		Pagination Pagination `json:"pagination"`
+	}
 
-type Pagination struct {
-	Total       int   `json:"total"`
-	Count       int   `json:"count"`
-	PerPage     int   `json:"per_page"`
-	CurrentPage int   `json:"current_page"`
-	TotalPages  int   `json:"total_pages"`
-	Links       Links `json:"links"`
-}
+	Pagination struct {
+		Total       int   `json:"total"`
+		Count       int   `json:"count"`
+		PerPage     int   `json:"per_page"`
+		CurrentPage int   `json:"current_page"`
+		TotalPages  int   `json:"total_pages"`
+		Links       Links `json:"links"`
+	}
 
-type Links struct {
-	Next string `json:"next"`
-}
+	Links struct {
+		Next string `json:"next"`
+	}
+)
