e398ada94bb3b0499126943ebc7692ec334977a6
armandasuryan <armanda.surya97@gmail.com>
2024-09-13 13:40:12 +0700

    fix name table

--- utils/table.go
+++ utils/table.go
@@ -1,7 +1,7 @@
 package utils
 
 const (
-	TABEL_USER                 = "user"
+	TABEL_USER                 = "users"
 	TABEL_EMPLOYEE             = "employee"
 	TABEL_ROLE                 = "role"
 	TABEL_EMPLOYEE_HAS_PROFILE = "employee_has_profile"
